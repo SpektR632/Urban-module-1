cc6659c7e2c6c8c17a0e79a7a38ff211fb6bcdde
SpektR632 <spektr.632@yandex.ru>
2024-05-16 07:29:21 +0300

    "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ homework4.py
@@ -0,0 +1,6 @@
+immutable_var = (1, True, 'фигура', ['20', 4])
+print(immutable_var)
+# immutable_var[0] = 20 # Кортеж является неизменяемым типом данных
+mutable_list = [4, False, 'супер']
+mutable_list[2] = 'элемент списка'
+print(mutable_list)
