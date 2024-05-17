5d3fed4692d7987b7020517851cf57ddc41abfa1
SpektR632 <spektr.632@yandex.ru>
2024-05-17 17:25:36 +0300

    "Списки и словари"

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['яблоко', 'груша', 'банан', 'киви']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:4])
+my_list[2] = 'апельсин'
+print(my_list)
+my_dict = {'яблоко': 'apple', 'апельсин': 'orange', 'банан': 'banana', 'киви': 'kiwi'}
+print(my_dict)
+print(my_dict['яблоко'])
+my_dict['груша'] = 'pear'
+print(my_dict)
