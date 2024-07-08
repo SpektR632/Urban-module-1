769f619a3f337bccbd4b5056a04506164dc3663a
SpektR632 <spektr.632@yandex.ru>
2024-07-08 07:07:40 +0300

    "Форматирование строк"

---
+++ homework7-4.py
@@ -0,0 +1,15 @@
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %s !' % team1_num)
+print('Итого сегодня в командах участников: %s и %s !' % (team1_num, team2_num))
+score_2 = 42
+print('Команда Волшебников данных решила задач: {} !'.format(score_2))
+team2_time = 2153.31451
+team1_time = 1552.512
+print('Волшебники данных решили задачи за {} c !'.format(team2_time))
+score_1 = 40
+print(f"Команды решили {score_1} и {score_2} задач.")
+challenge_result = 'Победа команды Волшебники данных!'
+print(f"Результат битвы: {challenge_result}!")
+time_avg = 45.2
+print(f"Сегодня было решено {score_1 + score_2}, в среднем по {time_avg} секунды на задачу!")
