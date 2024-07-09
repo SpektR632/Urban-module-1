4b4494df1e05571d5dcddaf9d57780866756088d
SpektR632 <spektr.632@yandex.ru>
2024-07-09 20:01:08 +0300

    Update homework7-5.py
    Исправлено в соответствии с заданием

--- homework7-5.py
+++ homework7-5.py
@@ -1,14 +1,12 @@
 import os
 from datetime import datetime
-for i in os.walk('.'):
-    print(i)
+for root, dirs, files in os.walk(r'C:\Users\Роман\PycharmProjects\pythonUrbanUniversity\Module 7'):
+    print(files)
+    for file in files:
+        filepath = os.path.dirname(__file__)
+        formatted_time = datetime.fromtimestamp(os.path.getmtime(file)).strftime('%d.%m.%Y %H:%M')
+        file_size = os.path.getsize(file)
+        parent_dir = os.path.dirname(__file__[os.path.dirname(__file__).rfind("\\")+1:])
 
-print(os.path.join('pythonUrbanUniversity', 'Module 7', 'homework7-5.py'))
-
-change_time = os.path.getmtime('homework7-5.py')
-
-print(datetime.fromtimestamp(change_time))
-
-print(os.path.getsize('homework7-5.py'))
-
-print(os.path.dirname(r'C:\Users\Роман\PycharmProjects\pythonUrbanUniversity'))
+        print(f"Обнаружен файл: {file}, Путь: {filepath}, Размер: {file_size} байт, Время изменения: {formatted_time},"
+              f" Родительская директория: {parent_dir}")
