b63646a7c66b522a77f7faf765adede6c03e7d9b
SpektR632 <spektr.632@yandex.ru>
2024-07-24 07:17:25 +0300

    Задача "Функциональное разнообразие"

---
+++ homework9-4.py
@@ -0,0 +1,28 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name,'w', encoding='utf-8') as file:
+            for i in data_set:
+                file.write(str(i)+'\n')
+    return write_everything
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+
+class MysticBall:
+    def __init__(self, *args):
+        self.words = args
+
+    def __call__(self):
+
+        return choice(self.words)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
