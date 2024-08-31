67a82d159a81ee80857de7beb4f773b4e7398975
SpektR632 <spektr.632@yandex.ru>
2024-08-31 15:31:47 +0300

    Задача "Проверка на выносливость"

---
+++ homework12-1.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        obj = Runner('boy')
+        for _ in range(10):
+            obj.walk()
+
+        self.assertEqual(obj.distance, 50)
+
+    def test_run(self):
+        obj = Runner('men')
+        for _ in range(10):
+            obj.run()
+
+        self.assertEqual(obj.distance, 100)
+
+    def test_callenge(self):
+        obj_1 = Runner('boy')
+        for _ in range(10):
+            obj_1.walk()
+
+        obj_2 = Runner('men')
+        for _ in range(10):
+            obj_2.run()
+
+        self.assertNotEqual(obj_1.distance, obj_2.distance)
+
