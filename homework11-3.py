d504fc65061528eac0dbbf31d4f644aa511f03c2
SpektR632 <spektr.632@yandex.ru>
2024-09-01 20:28:05 +0300

    homework11-3.py

--- homework11-3.py
+++ homework11-3.py
@@ -2,7 +2,7 @@ import inspect
 
 
 def introspection_info(object):
-    info = {'type': type(object), 'attributes': object.__dir__(),
+    info = {'type': type(object), 'attributes': [i for i in object.__dir__() if i not in dir(object)],
             'methods': dir(object),
             'module': inspect.getmodule(object, 'homework11-3.py')}
     return info
