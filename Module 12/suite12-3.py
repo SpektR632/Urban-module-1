0d5e90bd45018dcb5072fd1144a5b0b3f505aa29
SpektR632 <spektr.632@yandex.ru>
2024-09-03 17:21:57 +0300

    Update suite12-3.py

--- Module 12/suite12-3.py
+++ Module 12/suite12-3.py
@@ -5,8 +5,8 @@ import homework12_3_1
 import homework12_3_2
 
 runST = unittest.TestSuite()
-runST.addTest(unittest.TestLoader().loadTestsFromTestCase(homework12_1.RunnerTest))
-runST.addTest(unittest.TestLoader().loadTestsFromTestCase(homework12_2.TournamentTest))
+runST.addTest(unittest.TestLoader().loadTestsFromTestCase(homework12_3_1.RunnerTest))
+runST.addTest(unittest.TestLoader().loadTestsFromTestCase(homework12_3_2.TournamentTest))
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(runST)
